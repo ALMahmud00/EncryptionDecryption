9b48dc5915a10a8014403858ed2c676f294eab00
ALMahmud00 <al.mahmud00777@gmail.com>
2021-12-02 19:24:17 +0600

    warm up

---
+++ EncryptionDecryption/Controllers/DataController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EncryptionDecryption.Controllers
+{
+    [Route("protection/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        private readonly IDataProtector _dataProtector;
+        public DataController(IDataProtectionProvider _dataProtector)
+        {
+            this._dataProtector = _dataProtector.CreateProtector("@aV#2g7$fwaaciabidsssqp!jkad5");
+        }
+        [HttpGet]
+        public IActionResult Index(string plainText)
+        {
+            var encryptedText = _dataProtector.Protect(plainText);
+            var decryptedText = _dataProtector.Unprotect(encryptedText);
+
+            return Ok(new { plainText, encryptedText, decryptedText });
+        }
+    }
+}
